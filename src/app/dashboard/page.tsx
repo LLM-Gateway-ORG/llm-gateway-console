ef7c380cd2362c18db1981d635c38a959786e9f3
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2024-11-07 13:34:06 +0530

    added redirect to dashboard page in base url

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,22 +1,5 @@
-'use client'
-
-import { useAuthGuard } from '@/hooks/useAuthGuard';
+"use client";
 
 export default function Dashboard() {
-  const user = useAuthGuard();
-  
-  if (!user) {
-    return (
-      <div className="flex justify-center items-center h-screen bg-gray-100">
-        <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-500"></div>
-      </div>
-    );
-  }
-  
-  return (
-    <>
-      <h2 className="text-2xl font-bold mb-4 text-gray-800">Welcome, {user.firstname} {user.lastname}!</h2>
-      <p className="text-gray-600">This is your dashboard. You can add more content here.</p>
-    </>
-  );
+  return <></>;
 }
